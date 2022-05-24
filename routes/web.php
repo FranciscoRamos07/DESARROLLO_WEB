23d696fbeddfaabb6dfa18300e97a6a0565f9af9
FranciscoRamos07 <franciscoramos0709@gmail.com>
2022-05-24 12:45:07 -0700

    CRUD prueba

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+
+$router->get('/saludo',function(){
+    return 'Hola mi nombre es Francisco Ramos';
+});
+
+
+$router->post('/saludo', function(){
+    return 'Hola Mi nombre es francisco';
+
+});
+
+
+$router->put('/saludo',function(){
+    return 'hola mundo';
+});
+
+$router->delete('/saludo',function(){
+    return 'hola que tal';
+});
